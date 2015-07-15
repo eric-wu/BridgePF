7f71e214cbd61d1447a936f648cca2cb4b75352c
eric.wu <eric.wu@sagebase.org>
2015-07-15 15:29:32 -0700

    Makes extract more generic

--- extract.py
+++ extract.py
@@ -1,15 +1,18 @@
-""" Extracts and dedupes health codes """
+""" Extracts, dedupes, and outputs data to lines """
 
 import json
 
 inputfile = 'health-data-staging-merged'
+outputfile = 'upload-list-uat'
+field = 'uploadId'
+
 hcset = set()
 with open(inputfile, 'r') as lines:
     records = [json.loads(line) for line in lines]
     for record in records:
-        hcset.add(record['healthCode'])
+        hcset.add(record[field])
 
-with open('health-code-staging', 'w') as outputfile:
+with open(outputfile, 'w') as outputfile:
     for hc in hcset:
         outputfile.write(hc)
         outputfile.write('\n')
